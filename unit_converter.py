cb03753b4d85422cb08e90fb081a902b458d7fcc
maksmarn <63792965+maksmarn@users.noreply.github.com>
2021-04-14 17:27:28 +0200

    unit_converter.py
    A couple of corrections.

--- unit_converter.py
+++ unit_converter.py
@@ -3,11 +3,11 @@ print("Hello and welcome to Unit Converter!")
 while True:
     km = int(input("Please enter a number you wish to convert from km to miles: "))
 
-    print(str(km) + " kilometres is equal to "
-          + str(km * 0.621371) + " miles.")
-    yesorno = input("Would you like to convert some more? (yes/no) ")
-    if yesorno.lower() == "no":
-        break
-    elif yesorno.lower() != "yes":
-        print("ERROR")
+    print(f"{km} kilometres is equal to {km * 0.621371} miles.")
+    should_convert_again_string = input("Would you like to convert some more? (yes/no) ")
+    should_convert_again = should_convert_again_string == "yes"
+    if should_convert_again:
+        print("All right then.")
+    else:
+        print("Thank you for using Unit Converter.")
         break
